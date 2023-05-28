552e10c3a522a10f3e6efdf76be48b22ec3a2a4a
Shifa <121536886+ShifatJahanShifa@users.noreply.github.com>
2023-05-28 08:26:17 +0600

    added

---
+++ Sirprob-3.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#define F(X) (X*X)
+#define G(X) (2*X)
+#define MAXN 200
+
+int main()
+{
+	double a,b,tol;
+	a=0.5,b=5,tol=10.e-6;
+	
+	// bisection task
+	double m, fm,fa,fb;
+	fa=G(a)-F(a);
+	fb=G(b)-F(b);
+	
+	if(fa*fb>=0) 
+	{
+		puts("no root is here");
+	 	exit(1);
+	}
+	
+	puts("a      b       m       fa       fb        fm");
+	for(int i=1;i<=MAXN;i++)
+	{
+		m=a+(b-a)/2;
+		fm=G(m)-F(m);
+		
+		printf("%19.6lf%19.6lf%19.6lf%19.6lf%19.6lf%19.6lf\n",a,b,m,fa,fb,fm);
+		//printf("%lf,%lf\n",m,fm);
+		
+		if(fabs(fm)<=tol)
+		{
+			printf("found root. root is = %.6lf\n",m);
+			printf("iteration number = %d\n",i);
+			return EXIT_SUCCESS;
+		}
+		
+		if(fm*fa<0) 
+		{
+			b=m;
+			fb=fm;
+		}
+		else 
+		{
+			a=m;
+			fa=fm;
+		}	
+	}
+	
+	puts("iteration overflow");
+}
